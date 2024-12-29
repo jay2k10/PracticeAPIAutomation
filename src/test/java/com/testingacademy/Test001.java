725a87663c55c8d5b8e995c499a9a5defeb28d41
jay shankar <jayshankarprasad57@gmail.com>
2024-12-30 02:22:27 +0530

    Initial Commit

---
+++ src/test/java/com/testingacademy/Test001.java
@@ -0,0 +1,9 @@
+package com.testingacademy;
+
+import io.restassured.RestAssured;
+
+public class Test001 {
+    public static void main(String[] args) {
+        System.out.println("Hello Word");
+    }
+}
